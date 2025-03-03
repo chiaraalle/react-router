9ee7604e3cbaa70bbe4735db95b300903b33f8f5
chiaralle <chiaraallegrino@gmail.com>
2025-03-03 18:07:38 +0100

    add:posts

--- src/assets/Pages/Posts.jsx
+++ src/assets/Pages/Posts.jsx
@@ -1,4 +1,12 @@
+import { useState, useEffect } from "react";
+
 function Posts() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3000/posts")
+            .then((response) => response.json())
+            .then((data) => setPosts(data));
+    }, []);
     return (
         <div>
             <h1>Posts</h1>
