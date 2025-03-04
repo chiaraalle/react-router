6244b71e267c789f99e8073741e517d452abdc6b
chiaralle <chiaraallegrino@gmail.com>
2025-03-04 15:55:50 +0100

    add:file env  e map

--- src/assets/Pages/Posts.jsx
+++ src/assets/Pages/Posts.jsx
@@ -2,15 +2,35 @@ import { useState, useEffect } from "react";
 
 function Posts() {
     const [posts, setPosts] = useState([]);
+    const url = import.meta.env.VITE_ENDPOINT_URL /*il file .env permette di definire variabili che vengono poi usate nell'applicazione e mi permette di modificarli senza dover cambiare il codice */
+
+
     useEffect(() => {
-        fetch("http://localhost:3000/posts")
+        fetch( url )
             .then((response) => response.json())
             .then((data) => setPosts(data));
+
     }, []);
+
+
     return (
-        <div>
+        <>
             <h1>Posts</h1>
-        </div>
+            <div className="container-posts">
+                <div className="cards-post">
+                    {posts.map((post) => (
+                        <div key={post.id} className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{post.title}</h5>
+                                <p className="card-text">{post.content}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                
+            </div>
+            
+        </>
     )
 }
 
